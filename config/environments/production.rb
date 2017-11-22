67d62457766002f08f8b43eff7b48d4e676b7418
Zepri <clem.collongues@gmail.com>
2017-11-22 12:09:06 +0100

    typo fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,9 @@ Rails.application.configure do
   config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
+  config.assets.enabled = true
+  config.assets.paths << Rails.root.join('/app/assets/fonts')
+  config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
